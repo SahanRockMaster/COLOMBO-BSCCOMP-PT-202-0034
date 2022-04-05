1ad9510e66c71808585a4948ba920631c9d31345
SahanRockMaster <cobsccomp202p003@student.nibm.lk>
2022-04-06 01:22:03 +0530

    Initial Commit

---
+++ COLOMBO-BSCCOMP-PT-202-0034/COLOMBO_BSCCOMP_PT_202_0034App.swift
@@ -0,0 +1,17 @@
+//
+//  COLOMBO_BSCCOMP_PT_202_0034App.swift
+//  COLOMBO-BSCCOMP-PT-202-0034
+//
+//  Created by Sahan Ruwanga on 2022-04-06.
+//
+
+import SwiftUI
+
+@main
+struct COLOMBO_BSCCOMP_PT_202_0034App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COLOMBO-BSCCOMP-PT-202-0034/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COLOMBO-BSCCOMP-PT-202-0034
+//
+//  Created by Sahan Ruwanga on 2022-04-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
